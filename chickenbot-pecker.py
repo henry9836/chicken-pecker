7c8b361dc4b2da70c092ef1ade3031ba920b9c2f
Henry Oliver <37818378+henry9836@users.noreply.github.com>
2022-04-05 03:34:58 +1200

    Fixed a couple error conditions

--- chickenbot-pecker.py
+++ chickenbot-pecker.py
@@ -24,7 +24,19 @@ def clickAt(x, y, delay):
 
 def loop():
     while True:
-        ErrorMessage = pyautogui.locateCenterOnScreen('./chicken-vision/opps.png', grayscale=True, confidence=0.9)
+        ErrorMessage = pyautogui.locateCenterOnScreen('./chicken-vision/bad_down.png', grayscale=False, confidence=0.8)
+        if ErrorMessage != None:
+            print("CLUCK I FOUND A BAD CHICKEN!")
+            x = ErrorMessage.x
+            y = ErrorMessage.y + 100
+            clickAt(x, y, 0.2)
+        ErrorMessage = pyautogui.locateCenterOnScreen('./chicken-vision/bad_down2.png', grayscale=False, confidence=0.8)
+        if ErrorMessage != None:
+            print("CLUCK I FOUND A BAD CHICKEN!")
+            x = ErrorMessage.x
+            y = ErrorMessage.y + 100
+            clickAt(x, y, 0.2)
+        ErrorMessage = pyautogui.locateCenterOnScreen('./chicken-vision/opps.png', grayscale=True, confidence=0.8)
         if ErrorMessage != None:
             print("CLUCK I FOUND A BAD CHICKEN!")
             x = ErrorMessage.x
@@ -32,20 +44,37 @@ def loop():
             pyautogui.moveTo(x, y, duration = 3)
             clickAt(x, y, 0.2)
             pyautogui.hotkey('ctrl', 'r')
+            i = 0
             while True:
                 print("Waiting on Chicken...")
-                PlaceImage = pyautogui.locateCenterOnScreen('./chicken-vision/place.png', region=(x-100, y-75, 200, 75), grayscale=True, confidence=0.9)
+                PlaceImage = pyautogui.locateCenterOnScreen('./chicken-vision/place.png', region=(x-100, y-75, 200, 75), grayscale=True, confidence=0.8)
                 if PlaceImage != None:
                     clickAt(PlaceImage.x, PlaceImage.y, 0.2)
                     print("Chicken Fixed Returning To Watch...")
                     break
                 
-                Timer = pyautogui.locateCenterOnScreen('./chicken-vision/timer.png', region=(x-100, y-75, 200, 75), grayscale=True, confidence=0.9)
+                Timer = pyautogui.locateCenterOnScreen('./chicken-vision/timer.png', region=(x-100, y-75, 200, 75), grayscale=True, confidence=0.8)
                 if Timer != None:
                     clickAt(Timer.x, Timer.y, 0.2)
                     print("Chicken Fixed Returning To Watch...")
                     break
                 sleep(1)
+                i += 1
+                if i > 10:
+                    PlaceImage = pyautogui.locateCenterOnScreen('./chicken-vision/place.png', region=(x-100, y-200, 200, 200), grayscale=True, confidence=0.8)
+                if PlaceImage != None:
+                    clickAt(PlaceImage.x, PlaceImage.y, 0.2)
+                    print("Chicken Fixed Returning To Watch...")
+                    break
+                
+                Timer = pyautogui.locateCenterOnScreen('./chicken-vision/timer.png', region=(x-100, y-200, 200, 200), grayscale=True, confidence=0.8)
+                if Timer != None:
+                    clickAt(Timer.x, Timer.y, 0.2)
+                    print("Chicken Fixed Returning To Watch...")
+                    break
+                if i > 20:
+                    clickAt(x, PlaceImage.y, 0.2)
+                    break
         sleep(1)
 
 print(BANNER)
